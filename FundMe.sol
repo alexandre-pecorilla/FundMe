b7a4ed528197217a731fb2bd2cb711bc5a6ebb19
alexandre-pecorilla <alexandre@pecorilla.us>
2026-03-06 13:27:07 -0500

    msg.sender - storing funders and amounts

--- FundMe.sol
+++ FundMe.sol
@@ -16,7 +16,14 @@ contract FundMe {
     // the minimum amount in USD we want to require for calling the fund function
     // we have to add 18 zeros to it because solidity doesnt have floating numbers and speaks in wei, where 1 ETH = 1 * 1e18
     // so 5 dollars is represented as 5000000000000000000 which is equivalent to $5.000000000000000000, so 18 decimals
-    uint256  public minimumUSD = 5 * 1e18; 
+    uint256  public minimumUSD = 5 * 1e18; // can also be 5e18
+
+    // array of addresses that sent us money
+    address[] public funders;
+
+    mapping(address funder => uint256 amountFunded) public addresssToAmountFunded;
+
+
     // payable function
     // you can send eth to this function to fund the smart contract
     function fund() public payable { 
@@ -26,6 +33,8 @@ contract FundMe {
         // Therefore we will need to call an oracle to get the spot price of ethereum in ETH
         // That way we can require a minimum amount of ETH that matches minimumUSD
         require(getConversionRate(msg.value) >= minimumUSD, "You must sent at least $5.");
+        funders.push(msg.sender); // store the address of the sender to the array
+        addresssToAmountFunded[msg.sender] += msg.value; // store the amount sent by the sender
     }
 
     // function to fetch the live price of ethereum from the Chainlink Sepolia reference contract
